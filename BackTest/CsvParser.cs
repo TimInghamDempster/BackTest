e7800a71f19724acbbc4a5eb4f1d2f198854fe25
Tim Ingham-Dempster <tim_ingham_dempster@hotmail.co.uk>
2024-02-12 23:56:10 +0000

    Refactor: split up CSV parsing function

--- BackTest/CsvParser.cs
+++ BackTest/CsvParser.cs
@@ -2,19 +2,31 @@
 {
     internal static class CsvParser
     {
-        // Arguably too complex for a single linq statement, think
-        // about breaking this down into smaller methods
         internal static CompanyData Parse(IEnumerable<string> csvData) =>
-            new (
-                csvData.Skip(1).
-                Select(s => s.Split(',').First()).
-                Select(d =>
-                {
-                    var success = DateTime.TryParse(d, out var res);
-                    DateTime? dateTime = success ? res : null;
-                    return dateTime;
-                }).
-                Where(r => r.HasValue).
-                Select(r => r!.Value).ToList());
+            new(csvData.GetDates());
+
+        private static IEnumerable<DateTime> GetDates(this IEnumerable<string> csvData) =>
+            csvData.RemoveHeader().
+            GetFirstColumns().
+            ParseDates().
+            RemoveNulls();
+
+        // TODO: Move to common library at some point
+        private static IEnumerable<T> RemoveNulls<T>(this IEnumerable<T?> source) where T : struct =>
+            source.Where(r => r.HasValue).Select(r => r!.Value);
+
+        private static IEnumerable<string> GetFirstColumns(this IEnumerable<string> csvData) =>
+            csvData.Select(s => s.Split(',').First());
+
+        private static IEnumerable<DateTime?> ParseDates(this IEnumerable<string> source) =>
+            source.Select(d =>
+            {
+                var success = DateTime.TryParse(d, out var res);
+                DateTime? dateTime = success ? res : null;
+                return dateTime;
+            });
+
+        private static IEnumerable<string> RemoveHeader(this IEnumerable<string> csvData) =>
+             csvData.Skip(1);
     }
 }
